ac4ce526a70576c195dc08e353959ba97b602e5a
Parker Sornberger <parkersornberger3@gmail.com>
2025-07-08 03:42:47 -0400

    Update cStrideLinAlg.h

--- SkeletonsInC/cStrideLinAlg.h
+++ SkeletonsInC/cStrideLinAlg.h
@@ -12,7 +12,8 @@ typedef struct {
     Array** Arr2;
     Array** Arr3;
 } ArrTuple_3;
-
+void FreeDoubleTuple(ArrTuple_2* ret);
+void FreeTripleTuple(ArrTuple_3* ret);
 Array* proj(Array* v1, Array* v2);
 Array* Givens(size_t row1, size_t row2, size_t col, Array* mat);
 Array* mult_for_Givens(Array* A, Array* B, size_t g_i);
